37e9193281aefcc94aa450162234a098d7bc8d4b
Kevin Morillo <kevinkemuelm448@gmail.com>
2025-07-27 17:19:31 -0400

    ajuste paddin

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -76,7 +76,7 @@ const HeroSection = () => {
       </div>
       
       {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+      <div className="absolute bottom-8 transform -translate-x-1/2 animate-bounce">
         <div className="w-6 h-10 border-2 border-primary-foreground/50 rounded-full flex justify-center">
           <div className="w-1 h-3 bg-primary-foreground/50 rounded-full mt-2"></div>
         </div>
